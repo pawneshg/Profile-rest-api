a3f8156c3e8a0aaea685553c81406aa43e0ab6dc
pawneshg <pawneshg@gmail.com>
2018-03-31 18:24:37 +0530

    added CRUD methods to viewset

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -53,6 +53,8 @@ class HelloApiView(APIView):
 class HelloViewSet(viewsets.ViewSet):
     """test API viewsets."""
 
+    serializer_class = serializers.HelloSerializer
+
     def list(Self, request):
         """Return a Hello message."""
 
@@ -62,3 +64,35 @@ class HelloViewSet(viewsets.ViewSet):
             'provides more functionality with less code.'
         ]
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def  create(self, request):
+        """Create a new Hello message."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handles getiing an object by its ID."""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'http-method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'http-method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handles removing an object."""
+
+        return Response({'http-method': 'DELETE'})
